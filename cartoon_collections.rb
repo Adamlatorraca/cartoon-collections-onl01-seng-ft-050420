3ed1b31d92419c56d9aeb256969c0db92d17c8b1
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 21:00:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,9 @@ def summon_captain_planet(array)
   called
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+    array.any? { |i| i.length > 4 }
+
 end
 
 def find_the_cheese# code an argument here
