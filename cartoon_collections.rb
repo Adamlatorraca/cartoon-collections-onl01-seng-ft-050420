a3278fd552e689c7d1eb3c43c0438a8e465e2a6e
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 20:35:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map {|captain| puts captain.capitalize + "!"  }
+  array.map {|captain| puts captain.capitalize + "!"  }.to_a
 end
 
 def long_planeteer_calls# code an argument here
