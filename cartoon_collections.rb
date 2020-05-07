5020aa1801165d6171fcfcf1fd49c62a3c9d968f
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 20:39:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   called = []
-  planeteer_calls.map {|elem| elem.capitalize + "!"}
+  called << planeteer_calls.map {|elem| elem.capitalize + "!"}
 
 end
 
