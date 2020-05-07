6aab7be058ce4db57af7d459d3a8aa04b9821169
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 20:21:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,12 @@
-	dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(array)
   i = 0
 	array.each_with_object do |i|
-		puts "#{array[i]""
+		puts "#{array[i]}"
     i +=1
   end
+
 end
 
 def summon_captain_planet# code an argument here
