98f3ffee4a01084c176affe46483c46c341534fb
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 20:36:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def roll_call_dwarves(dwarves)
   i = 0
   while i < dwarves.length
-    puts "#{i + 1}. #{array[i]}"
+    puts "#{i + 1}. #{dwarves[i]}"
     i += 1
   end
 end
