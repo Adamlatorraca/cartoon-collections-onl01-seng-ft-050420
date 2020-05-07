6b44a69570ca7cabec5f63d308c2512778d5fab8
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 20:30:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,16 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def roll_call_dwarves(dwarves)
-  i = 0
-  while i < dwarves.length
-    puts "#{i + 1}. #{dwarves[i]}"
-    i += 1
-  end
-end
-
-def summon_captain_planet(array)
+def roll_call_dwarves(array)
   i = 0
   while i < array.length
     puts "#{i + 1}. #{array[i]}"
     i += 1
   end
+end
+
+def summon_captain_planet(array)
+
 end
 
 def long_planeteer_calls# code an argument here
